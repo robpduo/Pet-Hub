1de1cdc03892eb7805e16341d8f9091a06b6f0f9
robpduo <101683611+robpduo@users.noreply.github.com>
2022-08-10 19:49:41 -0400

    initial files

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
 const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(PORT, () => console.log(`Now Listening`));
