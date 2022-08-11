33e5f1a2f088b8fcf7979bcdb7e34f1991769193
robpduo <101683611+robpduo@users.noreply.github.com>
2022-08-10 20:23:56 -0400

    find all pet info api route

--- controllers/api/pet-routes.js
+++ controllers/api/pet-routes.js
@@ -1,4 +1,21 @@
 const router = require('express').Router();
-const {Pet} = require('../../models');
+const { Pet } = require('../../models');
+
+router.get('/', (req, res) => {
+    Pet.findAll({
+        attributes: [
+            'id',
+            'name',
+            'gender',
+            'age',
+            'created_at'
+        ]
+    })
+        .then(dbPetData => res.json(dbPetData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
 
 module.exports = router;
