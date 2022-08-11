dc8c2f61b62ddc10d016febd43cdbf2822e1c40d
robpduo <101683611+robpduo@users.noreply.github.com>
2022-08-11 10:50:02 -0400

    update pet route

--- controllers/api/pet-routes.js
+++ controllers/api/pet-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Pet } = require('../../models');
+const { Pet, User } = require('../../models');
 
 // retrieve all pets
 router.get('/', (req, res) => {
@@ -11,6 +11,12 @@ router.get('/', (req, res) => {
             'age',
             'bread',
             'picture_url'
+        ],
+        include: [
+            {
+                modle: User,
+                attributes: ['id', 'username', 'email']
+            }
         ]
     })
         .then(dbPetData => res.json(dbPetData))
@@ -29,12 +35,18 @@ router.get('/:id', (req, res) => {
             'age',
             'bread'
         ],
+        include: [
+            {
+                modle: User,
+                attributes: ['id', 'username', 'email']
+            }
+        ],
         where: {
             id: req.params.id
         }
     })
         .then(dbPetData => {
-            if(!dbPetData) {
+            if (!dbPetData) {
                 res.status(404).json({ message: 'No pet found with this id' });
                 return;
             }
@@ -45,5 +57,49 @@ router.get('/:id', (req, res) => {
             res.status(500).json(err);
         });
 });
+
+// create a new pet, under the current user session
+router.post('/', (req, res) => {
+    Pet.create(
+        {
+            name: req.body.name,
+            picture_url: req.body.picture_url,
+            age: req.body.age,
+            gender: req.body.gender,
+            bread: req.body.bread,
+            user_id: req.session.user_id // user id taken from user session
+        }
+    )
+        .then(dbPetData => res.json(dbPetData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.put('/:id', (req, res) => {
+    Pet.update(
+        {
+            name: req.body.name,
+            picture_url: req.body.picture_url,
+            age: req.body.age,
+            gender: req.body.gender,
+            bread: req.body.bread,
+        }
+    )
+    .then(dbPetData => {
+        if(!dbPetData) {
+            res.status(404).json({ message: 'No pets found with this id!'});
+            return;
+        }
+
+        res.json(dbPetData)
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+})
+
 
 module.exports = router;
