1f1eecc76e7de3049c6890b7f1c97d28282a21af
robpduo <101683611+robpduo@users.noreply.github.com>
2022-08-10 20:15:25 -0400

    initial pet model created

--- models/Pets.js
+++ models/Pets.js
@@ -1,7 +1,38 @@
 const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
 
 class Pet extends Model { }
 
-Pet.init({
-    
-})
+Pet.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        age: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        gender: {
+            type: DataTypes.CHAR,
+            validate: {
+                is: [F-M], //need to refactor to validate for either M or F
+                len: [1, 1] // validate of inpu is length 1 to 1
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        modelName: 'pet'
+    }
+);
+
+module.exports = Pet;
