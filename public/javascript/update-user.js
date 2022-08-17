ae6b22cd42b92897df4d62b3e0613b638077c034
robpduo <101683611+robpduo@users.noreply.github.com>
2022-08-17 10:16:20 -0400

    edit user gitignore avatar

--- public/javascript/update-user.js
+++ public/javascript/update-user.js
@@ -1,17 +1,19 @@
 async function updateUserHandler(event) {
     event.preventDefault();
 
-    const username = document.querySelector('#username-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const city = document.querySelector('#city-signup').value.trim();
+    const username = document.querySelector('#user-username').value.trim();
+    const email = document.querySelector('#user-email').value.trim();
+    const city = document.querySelector('#user-city').value.trim();
+    const password = document.querySelector('#user-password').value.trim();
 
     const formData = new FormData();
     const image = document.querySelector('input[type="file"]');
     formData.append('username', username);
+    formData.append('password', password);
     formData.append('email', email);
     formData.append('city', city);
     formData.append('image', image.files[0]);
-    
+
     const id = window.location.toString().split('/')[
         window.location.toString().split('/').length - 1
     ];
@@ -28,4 +30,4 @@ async function updateUserHandler(event) {
     }
 }
 
-document.querySelector('.update-btn').addEventListener('submit', updateUserHandler);
+document.querySelector('.edit-post-form').addEventListener('submit', updateUserHandler);
